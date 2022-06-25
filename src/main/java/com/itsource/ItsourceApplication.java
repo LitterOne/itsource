7c2e17552fd899c40b5ca1ec83bc882bcf7edaab
yh <501624244@qq.com>
2022-06-25 23:27:02 +0800

    项目初始化

---
+++ src/main/java/com/itsource/ItsourceApplication.java
@@ -0,0 +1,13 @@
+package com.itsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItsourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itsource/ItsourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
